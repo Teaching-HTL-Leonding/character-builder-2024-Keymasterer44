2f078bc8426ab1f8a4dc31d003b0fce2e67354c1
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-18 11:12:36 +0200

    forgot to commit this

---
+++ character-builder/src/app/randomize-character/randomize-character.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomize-character',
+  standalone: true,
+  imports: [],
+  templateUrl: './randomize-character.component.html',
+  styleUrl: './randomize-character.component.css'
+})
+export class RandomizeCharacterComponent {
+
+}
