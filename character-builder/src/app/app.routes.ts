7ec67fd3c1f963c9d4ab000a4cb47b9b3ee5f8c5
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-18 11:11:21 +0200

    added randomize-character route

--- character-builder/src/app/app.routes.ts
+++ character-builder/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {CreateCharacterComponent} from './create-character/create-character.component';
+import {RandomizeCharacterComponent} from './randomize-character/randomize-character.component';
 
 export const routes: Routes = [
   { path: "create", component: CreateCharacterComponent },
-  { path: "", redirectTo: "create", pathMatch: "full" }
+  { path: "", redirectTo: "create", pathMatch: "full" },
+  { path: "randomize", component: RandomizeCharacterComponent }
 ];
