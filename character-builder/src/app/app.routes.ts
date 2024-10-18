bd3515669a1685a03ddd0dbecf04c796cab5fd6b
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-18 09:22:58 +0200

    added path definitions

--- character-builder/src/app/app.routes.ts
+++ character-builder/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {CreateCharacterComponent} from './create-character/create-character.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "/create", component: CreateCharacterComponent },
+  { path: "/", redirectTo: "/create", pathMatch: "full" }
+];
