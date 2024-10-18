a8ddf86a7e5ae3109aa85c6266f27937dd232d3c
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-18 09:41:58 +0200

    added required service functionality

--- character-builder/src/app/create-character.service.ts
+++ character-builder/src/app/create-character.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {firstValueFrom} from 'rxjs';
+
+type Eye = "NoEye" | "HalfOpen" | "Closed" | "Open";
+type Mouth = "NoMouth" | "Happy" | "Normal" | "Unhappy";
+type RightHand = "NoHand" | "Normal" | "Victory";
+
+type BuildCharacterRequest = {
+  eye: Eye;
+  hasHammer: boolean;
+  mouth: Mouth;
+  rightHand: RightHand;
+  hasTail: boolean;
+}
+
+type BuildCharacterResponse = {
+  url: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateCharacterService {
+  private httpClient = inject(HttpClient);
 
   constructor() { }
+
+  buildCharacter(eye: Eye, hasHammer: boolean, mouth: Mouth, rightHand: RightHand, hasTail: boolean): Promise<BuildCharacterResponse> {
+    const reqBody: BuildCharacterRequest = {
+      eye,
+      hasHammer,
+      mouth,
+      rightHand,
+      hasTail
+    }
+    return firstValueFrom(
+      this.httpClient.post<BuildCharacterResponse>(
+        'http://localhost:5110/build-image-url',
+        reqBody
+      )
+    );
+  }
+
+  getRandomImageOptions(): Promise<BuildCharacterRequest> {
+    return firstValueFrom(
+      this.httpClient.get<BuildCharacterRequest>(
+        'http://localhost:5110/get-random-image-options'
+      )
+    )
+  }
+
+  getImage(url: string): Promise<string> {
+    return firstValueFrom(
+      this.httpClient.get<string>(
+        `http://localhost:5110/img/${url}`
+      )
+    )
+  }
 }
