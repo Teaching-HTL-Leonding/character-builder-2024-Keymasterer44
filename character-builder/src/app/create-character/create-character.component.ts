1c7fd40d82964033f001a7a6898801eab672940e
key44 <k.minihuber@students.htl-leonding.ac.at>
2024-10-18 10:48:58 +0200

    made the minimum stuff work

--- character-builder/src/app/create-character/create-character.component.ts
+++ character-builder/src/app/create-character/create-character.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
+import {CreateCharacterService, eyeOptions, mouthOptions, rightHandOptions} from '../create-character.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-character',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './create-character.component.html',
   styleUrl: './create-character.component.css'
 })
 export class CreateCharacterComponent {
+  private readonly createCharacterService = inject(CreateCharacterService);
+  readonly eyeOptions = eyeOptions;
+  readonly mouthOptions = mouthOptions;
+  readonly rightHandOptions = rightHandOptions;
 
+  eye = signal(eyeOptions[0]);
+  hasHammer = signal(false);
+  mouth = signal(mouthOptions[0]);
+  rightHand = signal(rightHandOptions[0]);
+  hasTail = signal(false);
+
+  imageUrl = signal<string | null>(null);
+
+  async createCharacter() {
+    const imageId = await this.createCharacterService.buildCharacter(
+      this.eye(),
+      this.hasHammer(),
+      this.mouth(),
+      this.rightHand(),
+      this.hasTail()
+    );
+    console.log(imageId);
+    this.imageUrl.set(imageId.url);
+  }
+
+  createRandomCharacter() {
+
+  }
 }
